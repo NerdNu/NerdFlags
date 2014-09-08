9f1b23ab209e09f9135c3195dbf87d34a6bc5288
Dumbo52 <dumbofiftytwo@gmail.com>
2014-09-07 22:53:45 -0400

    Initial commit.

---
+++ src/com/michaelelin/NerdFlags/NerdFlagsListener.java
@@ -0,0 +1,166 @@
+package com.michaelelin.NerdFlags;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+
+public class NerdFlagsListener implements Listener {
+    
+    private NerdFlagsPlugin plugin;
+    private WorldGuardPlugin worldguard;
+    private WorldEditPlugin worldedit;
+    
+    public NerdFlagsListener(NerdFlagsPlugin plugin, WorldGuardPlugin worldguard, WorldEditPlugin worldedit) {
+        this.plugin = plugin;
+        this.worldguard = worldguard;
+        this.worldedit = worldedit;
+    }
+    
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onItemSpawn(ItemSpawnEvent event) {
+        ApplicableRegionSet setAtLocation = worldguard.getGlobalRegionManager().get(event.getLocation().getWorld()).getApplicableRegions(event.getLocation());
+        if (!setAtLocation.allows(plugin.ALLOW_DROPS)) {
+            event.setCancelled(true);
+        }
+    }
+    
+ // WE checks this at a NORMAL priority, so we'll intercept it beforehand.
+    @EventHandler(priority = EventPriority.LOW)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getItem() != null && event.getItem().getTypeId() == worldedit.getWorldEdit().getConfiguration().navigationWand) {
+            plugin.expectTeleport(event.getPlayer());
+        }
+        if (!event.isCancelled() && event.getClickedBlock() != null) {
+            Location location = event.getClickedBlock().getLocation();
+            LocalPlayer player = worldguard.wrapPlayer(event.getPlayer());
+    
+            if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                switch (event.getClickedBlock().getType()) {
+                case DISPENSER:
+                    cancelEvent(event, !allows(plugin.USE_DISPENSER, location, player), true);
+                    break;
+                case NOTE_BLOCK:
+                    cancelEvent(event, !allows(plugin.USE_NOTE_BLOCK, location, player), true);
+                    break;
+                case WORKBENCH:
+                    cancelEvent(event, !allows(plugin.USE_WORKBENCH, location, player), true);
+                    break;
+                case WOODEN_DOOR:
+                    cancelEvent(event, !allows(plugin.USE_DOOR, location, player), true);
+                    break;
+                case LEVER:
+                    cancelEvent(event, !allows(plugin.USE_LEVER, location, player), true);
+                    break;
+                case STONE_BUTTON:
+                case WOOD_BUTTON:
+                    cancelEvent(event, !allows(plugin.USE_BUTTON, location, player), true);
+                    break;
+                case JUKEBOX:
+                    cancelEvent(event, !allows(plugin.USE_JUKEBOX, location, player), true);
+                    break;
+                case DIODE_BLOCK_OFF:
+                case DIODE_BLOCK_ON:
+                    cancelEvent(event, !allows(plugin.USE_REPEATER, location, player), true, true);
+                    break;
+                case TRAP_DOOR:
+                    cancelEvent(event, !allows(plugin.USE_TRAP_DOOR, location, player), true);
+                    break;
+                case FENCE_GATE:
+                    cancelEvent(event, !allows(plugin.USE_FENCE_GATE, location, player), true);
+                    break;
+                case BREWING_STAND:
+                    cancelEvent(event, !allows(plugin.USE_BREWING_STAND, location, player), true);
+                    break;
+                case CAULDRON:
+                    cancelEvent(event, !allows(plugin.USE_CAULDRON, location, player), true);
+                    break;
+                case ENCHANTMENT_TABLE:
+                    cancelEvent(event, !allows(plugin.USE_ENCHANTMENT_TABLE, location, player), true);
+                    break;
+                case ENDER_CHEST:
+                    cancelEvent(event, !allows(plugin.USE_ENDER_CHEST, location, player), true);
+                    break;
+                case BEACON:
+                    cancelEvent(event, !allows(plugin.USE_BEACON, location, player), true);
+                    break;
+                case ANVIL:
+                    cancelEvent(event, !allows(plugin.USE_ANVIL, location, player), true);
+                    break;
+                case REDSTONE_COMPARATOR_OFF:
+                case REDSTONE_COMPARATOR_ON:
+                    cancelEvent(event, !allows(plugin.USE_COMPARATOR, location, player), true, true);
+                    break;
+                case HOPPER:
+                    cancelEvent(event, !allows(plugin.USE_HOPPER, location, player), true);
+                    break;
+                case DROPPER:
+                    cancelEvent(event, !allows(plugin.USE_DROPPER, location, player), true);
+                    break;
+                default:
+                }
+            }
+            if (event.getAction() == Action.PHYSICAL) {
+                Material mat = event.getClickedBlock().getType();
+                if (mat == Material.STONE_PLATE || mat == Material.WOOD_PLATE || mat == Material.GOLD_PLATE || mat == Material.IRON_PLATE) {
+                    cancelEvent(event, !allows(plugin.USE_PRESSURE_PLATE, location, player), false);
+                }
+                else if (mat == Material.TRIPWIRE) {
+                    cancelEvent(event, !allows(plugin.USE_TRIPWIRE, location, player), false);
+                }
+            }
+        }
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        if (event.getMessage().equalsIgnoreCase("/jumpto") || event.getMessage().equalsIgnoreCase("/j") || event.getMessage().equalsIgnoreCase("/thru")) {
+            plugin.expectTeleport(event.getPlayer());
+        }
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
+        if (plugin.hasCompassed(event.getPlayer())) {
+            System.out.println("Has compassed");
+            ApplicableRegionSet setAtLocation = worldguard.getGlobalRegionManager().get(event.getFrom().getWorld()).getApplicableRegions(event.getFrom());
+            ApplicableRegionSet setAtTeleport = worldguard.getGlobalRegionManager().get(event.getTo().getWorld()).getApplicableRegions(event.getTo());
+            LocalPlayer player = worldguard.wrapPlayer(event.getPlayer());
+            if (!worldguard.getGlobalRegionManager().hasBypass(player, event.getPlayer().getWorld()) && (!setAtLocation.canBuild(player) && !setAtLocation.allows(plugin.COMPASS, player) || !setAtTeleport.canBuild(player) && !setAtTeleport.allows(plugin.COMPASS, player))) {
+                event.setCancelled(true);
+                event.getPlayer().sendMessage(ChatColor.DARK_RED + "You don't have permission to use that in this area.");
+            }
+        }
+    }
+    
+    private boolean allows(StateFlag flag, Location location, LocalPlayer player) {
+        ApplicableRegionSet set = worldguard.getGlobalRegionManager().get(location.getWorld()).getApplicableRegions(location);
+        return worldguard.getGlobalRegionManager().hasBypass(player, location.getWorld()) || set.canBuild(player) || set.allows(flag, player);
+    }
+
+    private void cancelEvent(PlayerInteractEvent e, boolean cancel, boolean notifyPlayer) {
+        cancelEvent(e, cancel, notifyPlayer, false);
+    }
+
+    // Override for repeaters and comparators, since WG cancels these without
+    // any checks.
+    private void cancelEvent(PlayerInteractEvent e, boolean cancel, boolean notifyPlayer, boolean override) {
+        e.setCancelled(cancel);
+        if (e.isCancelled() && notifyPlayer) {
+            e.getPlayer().sendMessage(ChatColor.DARK_RED + "You don't have permission to use that in this area.");
+        }
+    }
+}
---
+++ src/com/michaelelin/NerdFlags/NerdFlagsPlugin.java
@@ -0,0 +1,128 @@
+package com.michaelelin.NerdFlags;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.mewin.WGCustomFlags.WGCustomFlagsPlugin;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+
+public class NerdFlagsPlugin extends JavaPlugin {
+    
+    private NerdFlagsListener listener;
+    private Player nextTP;
+    private long timestamp;
+
+    public StateFlag ALLOW_DROPS;
+
+    public StateFlag COMPASS;
+    
+    public StateFlag USE_DISPENSER;
+    public StateFlag USE_NOTE_BLOCK;
+    public StateFlag USE_WORKBENCH;
+    public StateFlag USE_DOOR;
+    public StateFlag USE_LEVER;
+    public StateFlag USE_PRESSURE_PLATE;
+    public StateFlag USE_BUTTON;
+    public StateFlag USE_JUKEBOX;
+    public StateFlag USE_REPEATER;
+    public StateFlag USE_TRAP_DOOR;
+    public StateFlag USE_FENCE_GATE;
+    public StateFlag USE_BREWING_STAND;
+    public StateFlag USE_CAULDRON;
+    public StateFlag USE_ENCHANTMENT_TABLE;
+    public StateFlag USE_ENDER_CHEST;
+    public StateFlag USE_TRIPWIRE;
+    public StateFlag USE_BEACON;
+    public StateFlag USE_ANVIL;
+    public StateFlag USE_COMPARATOR;
+    public StateFlag USE_HOPPER;
+    public StateFlag USE_DROPPER;
+
+    @Override
+    public void onEnable() {
+        WGCustomFlagsPlugin wgCustomFlagsPlugin = getPlugin("WGCustomFlags", WGCustomFlagsPlugin.class);
+        WorldGuardPlugin worldGuardPlugin = getPlugin("WorldGuard", WorldGuardPlugin.class);
+        WorldEditPlugin worldEditPlugin = getPlugin("WorldEdit", WorldEditPlugin.class);
+
+        if (wgCustomFlagsPlugin != null && worldGuardPlugin != null && worldEditPlugin != null) {
+            listener = new NerdFlagsListener(this, worldGuardPlugin, worldEditPlugin);
+            getServer().getPluginManager().registerEvents(listener, this);
+            
+            ALLOW_DROPS = new StateFlag("allow-drops", true);
+            COMPASS = new StateFlag("compass", true);
+            wgCustomFlagsPlugin.addCustomFlag(ALLOW_DROPS);
+            wgCustomFlagsPlugin.addCustomFlag(COMPASS);
+            
+            saveDefaultConfig();
+            loadConfig();
+            wgCustomFlagsPlugin.addCustomFlag(USE_DISPENSER);
+            wgCustomFlagsPlugin.addCustomFlag(USE_NOTE_BLOCK);
+            wgCustomFlagsPlugin.addCustomFlag(USE_WORKBENCH);
+            wgCustomFlagsPlugin.addCustomFlag(USE_DOOR);
+            wgCustomFlagsPlugin.addCustomFlag(USE_LEVER);
+            wgCustomFlagsPlugin.addCustomFlag(USE_PRESSURE_PLATE);
+            wgCustomFlagsPlugin.addCustomFlag(USE_BUTTON);
+            wgCustomFlagsPlugin.addCustomFlag(USE_JUKEBOX);
+            wgCustomFlagsPlugin.addCustomFlag(USE_REPEATER);
+            wgCustomFlagsPlugin.addCustomFlag(USE_TRAP_DOOR);
+            wgCustomFlagsPlugin.addCustomFlag(USE_FENCE_GATE);
+            wgCustomFlagsPlugin.addCustomFlag(USE_BREWING_STAND);
+            wgCustomFlagsPlugin.addCustomFlag(USE_CAULDRON);
+            wgCustomFlagsPlugin.addCustomFlag(USE_ENCHANTMENT_TABLE);
+            wgCustomFlagsPlugin.addCustomFlag(USE_ENDER_CHEST);
+            wgCustomFlagsPlugin.addCustomFlag(USE_TRIPWIRE);
+            wgCustomFlagsPlugin.addCustomFlag(USE_BEACON);
+            wgCustomFlagsPlugin.addCustomFlag(USE_ANVIL);
+            wgCustomFlagsPlugin.addCustomFlag(USE_COMPARATOR);
+            wgCustomFlagsPlugin.addCustomFlag(USE_HOPPER);
+            wgCustomFlagsPlugin.addCustomFlag(USE_DROPPER);
+        }
+    }
+
+    private <T extends Plugin> T getPlugin(String name, Class<T> mainClass) {
+        Plugin plugin = getServer().getPluginManager().getPlugin(name);
+        if (plugin == null || !mainClass.isInstance(plugin)) {
+            getLogger().warning("[" + getName() + "] " + name + " is required for this plugin to work; disabling.");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+        return mainClass.cast(plugin);
+    }
+    
+    private void loadConfig() {
+        USE_DISPENSER = new StateFlag("use-dispenser", getConfig().getBoolean("default-dispenser"));
+        USE_NOTE_BLOCK = new StateFlag("use-note-block", getConfig().getBoolean("default-note-block"));
+        USE_WORKBENCH = new StateFlag("use-workbench", getConfig().getBoolean("default-workbench"));
+        USE_DOOR = new StateFlag("use-door", getConfig().getBoolean("default-door"));
+        USE_LEVER = new StateFlag("use-lever", getConfig().getBoolean("default-lever"));
+        USE_PRESSURE_PLATE = new StateFlag("use-pressure-plate", getConfig().getBoolean("default-pressure-plate"));
+        USE_BUTTON = new StateFlag("use-button", getConfig().getBoolean("default-button"));
+        USE_JUKEBOX = new StateFlag("use-jukebox", getConfig().getBoolean("default-jukebox"));
+        USE_REPEATER = new StateFlag("use-repeater", getConfig().getBoolean("default-repeater"));
+        USE_TRAP_DOOR = new StateFlag("use-trap-door", getConfig().getBoolean("default-trap-door"));
+        USE_FENCE_GATE = new StateFlag("use-fence-gate", getConfig().getBoolean("default-fence-gate"));
+        USE_BREWING_STAND = new StateFlag("use-brewing-stand", getConfig().getBoolean("default-brewing-stand"));
+        USE_CAULDRON = new StateFlag("use-cauldron", getConfig().getBoolean("default-cauldron"));
+        USE_ENCHANTMENT_TABLE = new StateFlag("use-enchantment-table", getConfig().getBoolean("default-enchantment-table"));
+        USE_ENDER_CHEST = new StateFlag("use-ender-chest", getConfig().getBoolean("default-ender-chest"));
+        USE_TRIPWIRE = new StateFlag("use-tripwire", getConfig().getBoolean("default-tripwire"));
+        USE_BEACON = new StateFlag("use-beacon", getConfig().getBoolean("default-beacon"));
+        USE_ANVIL = new StateFlag("use-anvil", getConfig().getBoolean("default-anvil"));
+        USE_COMPARATOR = new StateFlag("use-comparator", getConfig().getBoolean("default-comparator"));
+        USE_HOPPER = new StateFlag("use-hopper", getConfig().getBoolean("default-hopper"));
+        USE_DROPPER = new StateFlag("use-dropper", getConfig().getBoolean("default-dropper"));
+    }
+    
+    public void expectTeleport(Player player) {
+        this.nextTP = player;
+        this.timestamp = player.getPlayerTime();
+        System.out.println("Expecting teleport");
+    }
+    
+    public boolean hasCompassed(Player player) {
+        return this.nextTP == player && this.timestamp == player.getPlayerTime();
+    }
+
+}
