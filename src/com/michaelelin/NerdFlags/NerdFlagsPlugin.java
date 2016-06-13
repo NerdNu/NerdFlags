7e5783f1da216fdf45501e3c3e0a55f322d51b28
Challenger2 <challenger2@users.noreply.github.com>
2016-06-12 22:44:09 -0500

    Got NerdFlags working with stock WorldGuard

--- src/com/michaelelin/NerdFlags/NerdFlagsPlugin.java
+++ src/com/michaelelin/NerdFlags/NerdFlagsPlugin.java
@@ -1,6 +1,14 @@
 package com.michaelelin.NerdFlags;
 
 import com.sk89q.worldguard.protection.flags.*;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.logging.Level;
+
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -10,11 +18,8 @@ import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.flags.CustomFlagBroker;
-import com.sk89q.worldguard.protection.flags.CustomFlagProvider;
-import com.sk89q.worldguard.protection.flags.StateFlag;
 
-public class NerdFlagsPlugin extends JavaPlugin implements CustomFlagProvider {
+public class NerdFlagsPlugin extends JavaPlugin {
 
     private NerdFlagsListener listener;
     private NerdFlagsRegionListener regionListener;
@@ -110,44 +115,46 @@ public class NerdFlagsPlugin extends JavaPlugin implements CustomFlagProvider {
     }
 
     /**
-     * This is a call back from WorldGuard. It could be called at any time after onLaod(), even
-     * before onEnable()!!!
+     * Hack flags into world guard before any onLoad() of any plugin is called
      */
     @Override
-    public void addCustomFlags(CustomFlagBroker broker) {
+    public void onLoad() {
+    	
+    	Collection<Flag<?>> flags = new ArrayList<Flag<?>>();
+
         ALLOW_DROPS = new StateFlag("allow-drops", true);
-        broker.addFlag(ALLOW_DROPS);
+       flags.add(ALLOW_DROPS);
         
         ALLOW_MOB_DROPS = new StateFlag("allow-mob-drops", true);
-        broker.addFlag(ALLOW_MOB_DROPS);
+        flags.add(ALLOW_MOB_DROPS);
         
         // Using EntityTypeFlag here instead of EnumFlag causes WGCustomFlags to run into some reflection issues
         PLAYER_MOB_DAMAGE = new SetFlag<EntityType>("player-mob-damage", new EnumFlag<EntityType>("", EntityType.class));
-        broker.addFlag(PLAYER_MOB_DAMAGE);
+        flags.add(PLAYER_MOB_DAMAGE);
         
         NETHER_PORTAL = new StateFlag("nether-portal", true);
-        broker.addFlag(NETHER_PORTAL);
+        flags.add(NETHER_PORTAL);
         
         END_PORTAL = new StateFlag("end-portal", true);
-        broker.addFlag(END_PORTAL);
+        flags.add(END_PORTAL);
         
         SNOWBALL_FIREFIGHT = new StateFlag("snowball-firefight", false);
-        broker.addFlag(SNOWBALL_FIREFIGHT);
+        flags.add(SNOWBALL_FIREFIGHT);
         
         COMPASS = new StateFlag("compass", true);
-        broker.addFlag(COMPASS);
+        flags.add(COMPASS);
         
         WEATHER = new StateFlag("weather", false);
-        broker.addFlag(WEATHER);
+        flags.add(WEATHER);
         
         DATE = new StringFlag("date");
-        broker.addFlag(DATE);
+        flags.add(DATE);
         
         CREATED_BY = new StringFlag("created-by");
-        broker.addFlag(CREATED_BY);
+        flags.add(CREATED_BY);
         
         ENTRY_COMMANDS = new StringFlag("entry-commands");
-        broker.addFlag(ENTRY_COMMANDS);
+        flags.add(ENTRY_COMMANDS);
 
         // TODO: Fix this
         //WARP = new CustomLocationFlag("warp");
@@ -156,71 +163,111 @@ public class NerdFlagsPlugin extends JavaPlugin implements CustomFlagProvider {
         saveDefaultConfig();
         
         USE_DISPENSER = new StateFlag("use-dispenser", getConfig().getBoolean("default-dispenser"));
-        broker.addFlag(USE_DISPENSER);
+        flags.add(USE_DISPENSER);
         
         USE_NOTE_BLOCK = new StateFlag("use-note-block", getConfig().getBoolean("default-note-block"));
-        broker.addFlag(USE_NOTE_BLOCK);
+        flags.add(USE_NOTE_BLOCK);
         
         USE_WORKBENCH = new StateFlag("use-workbench", getConfig().getBoolean("default-workbench"));
-        broker.addFlag(USE_WORKBENCH);
+        flags.add(USE_WORKBENCH);
         
         USE_DOOR = new StateFlag("use-door", getConfig().getBoolean("default-door"));
-        broker.addFlag(USE_DOOR);
+        flags.add(USE_DOOR);
         
         USE_LEVER = new StateFlag("use-lever", getConfig().getBoolean("default-lever"));
-        broker.addFlag(USE_LEVER);
+        flags.add(USE_LEVER);
         
         USE_PRESSURE_PLATE = new StateFlag("use-pressure-plate", getConfig().getBoolean("default-pressure-plate"));
-        broker.addFlag(USE_PRESSURE_PLATE);
+        flags.add(USE_PRESSURE_PLATE);
         
         USE_BUTTON = new StateFlag("use-button", getConfig().getBoolean("default-button"));
-        broker.addFlag(USE_BUTTON);
+        flags.add(USE_BUTTON);
         
         USE_JUKEBOX = new StateFlag("use-jukebox", getConfig().getBoolean("default-jukebox"));
-        broker.addFlag(USE_JUKEBOX);
+        flags.add(USE_JUKEBOX);
         
         USE_REPEATER = new StateFlag("use-repeater", getConfig().getBoolean("default-repeater"));
-        broker.addFlag(USE_REPEATER);
+        flags.add(USE_REPEATER);
         
         USE_TRAP_DOOR = new StateFlag("use-trap-door", getConfig().getBoolean("default-trap-door"));
-        broker.addFlag(USE_TRAP_DOOR);
+        flags.add(USE_TRAP_DOOR);
         
         USE_FENCE_GATE = new StateFlag("use-fence-gate", getConfig().getBoolean("default-fence-gate"));
-        broker.addFlag(USE_FENCE_GATE);
+        flags.add(USE_FENCE_GATE);
         
         USE_BREWING_STAND = new StateFlag("use-brewing-stand", getConfig().getBoolean("default-brewing-stand"));
-        broker.addFlag(USE_BREWING_STAND);
+        flags.add(USE_BREWING_STAND);
         
         USE_CAULDRON = new StateFlag("use-cauldron", getConfig().getBoolean("default-cauldron"));
-        broker.addFlag(USE_CAULDRON);
+        flags.add(USE_CAULDRON);
         
         USE_ENCHANTMENT_TABLE = new StateFlag("use-enchantment-table", getConfig().getBoolean("default-enchantment-table"));
-        broker.addFlag(USE_ENCHANTMENT_TABLE);
+        flags.add(USE_ENCHANTMENT_TABLE);
         
         USE_ENDER_CHEST = new StateFlag("use-ender-chest", getConfig().getBoolean("default-ender-chest"));
-        broker.addFlag(USE_ENDER_CHEST);
+        flags.add(USE_ENDER_CHEST);
         
         USE_TRIPWIRE = new StateFlag("use-tripwire", getConfig().getBoolean("default-tripwire"));
-        broker.addFlag(USE_TRIPWIRE);
+        flags.add(USE_TRIPWIRE);
         
         USE_BEACON = new StateFlag("use-beacon", getConfig().getBoolean("default-beacon"));
-        broker.addFlag(USE_BEACON);
+        flags.add(USE_BEACON);
         
         USE_ANVIL = new StateFlag("use-anvil", getConfig().getBoolean("default-anvil"));
-        broker.addFlag(USE_ANVIL);
+        flags.add(USE_ANVIL);
         
         USE_COMPARATOR = new StateFlag("use-comparator", getConfig().getBoolean("default-comparator"));
-        broker.addFlag(USE_COMPARATOR);
+        flags.add(USE_COMPARATOR);
         
         USE_HOPPER = new StateFlag("use-hopper", getConfig().getBoolean("default-hopper"));
-        broker.addFlag(USE_HOPPER);
+        flags.add(USE_HOPPER);
         
         USE_DROPPER = new StateFlag("use-dropper", getConfig().getBoolean("default-dropper"));
-        broker.addFlag(USE_DROPPER);
+        flags.add(USE_DROPPER);
         
         USE_DAYLIGHT_DETECTOR = new StateFlag("use-daylight-detector", getConfig().getBoolean("default-daylight-detector"));
-        broker.addFlag(USE_DAYLIGHT_DETECTOR);
+        flags.add(USE_DAYLIGHT_DETECTOR);
         
+        addFlags(flags);    
+        getLogger().log(Level.INFO, "Loaded all flags");
+    }
+
+
+    /**
+     * Add a collection of flags to WorldGuard
+     * 
+     * @param flags Flags to add
+     */
+    public void addFlags(Collection<Flag<?>> flags) {
+
+    	for (Flag<?> flag : flags) {
+	        Flag<?> match = DefaultFlag.fuzzyMatchFlag(flag.getName());
+	        if (match != null) {
+	            throw new IllegalArgumentException("Duplicate flag");
+	        }
+    	}
+
+        Flag<?>[] newList = Arrays.copyOf(DefaultFlag.flagsList, DefaultFlag.flagsList.length + flags.size());
+        int i = 0;
+        for (Flag<?> flag : flags) {
+        	newList[DefaultFlag.flagsList.length + i] = flag;
+        	i++;
+        }
+
+        // Force update the flagsList
+        try {
+            Field field = DefaultFlag.class.getField("flagsList");
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+            field.set(null, newList);
+        } catch (SecurityException e) {
+        	getLogger().log(Level.WARNING, "A Security Policy prevented setting the custom flag", e);
+        } catch (IllegalAccessException e) {
+            getLogger().log(Level.WARNING, "Failed to overwrite DefaultFlag.flagsList", e);
+        } catch (NoSuchFieldException e) {
+        	getLogger().log(Level.WARNING, "Failed to find a field", e);
+        }
     }
 
 }
